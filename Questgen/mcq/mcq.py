225fe5bb92b88d9d24547f29c305694219435300
Kenny Valdivia <k_valdivia@gmx.com>
2024-08-07 22:00:32 -0500

    Fix model dependencies

--- Questgen/mcq/mcq.py
+++ Questgen/mcq/mcq.py
@@ -1,8 +1,8 @@
-import numpy as np # linear algebra
-import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import numpy as np  # linear algebra
+import pandas as pd  # data processing, CSV file I/O (e.g. pd.read_csv)
 import time
 import torch
-from transformers import T5ForConditionalGeneration,T5Tokenizer
+from transformers import T5ForConditionalGeneration, T5Tokenizer
 import random
 import spacy
 import zipfile
@@ -15,16 +15,14 @@ import string
 import pke
 import nltk
 from nltk import FreqDist
-nltk.download('brown')
-nltk.download('stopwords')
-nltk.download('popular')
 from nltk.corpus import stopwords
 from nltk.corpus import brown
 from similarity.normalized_levenshtein import NormalizedLevenshtein
 from nltk.tokenize import sent_tokenize
 from flashtext import KeywordProcessor
 
-def MCQs_available(word,s2v):
+
+def MCQs_available(word, s2v):
     word = word.replace(" ", "_")
     sense = s2v.get_best_sense(word)
     if sense is not None:
@@ -35,19 +33,20 @@ def MCQs_available(word,s2v):
 
 def edits(word):
     "All edits that are one edit away from `word`."
-    letters    = 'abcdefghijklmnopqrstuvwxyz '+string.punctuation
-    splits     = [(word[:i], word[i:])    for i in range(len(word) + 1)]
-    deletes    = [L + R[1:]               for L, R in splits if R]
-    transposes = [L + R[1] + R[0] + R[2:] for L, R in splits if len(R)>1]
-    replaces   = [L + c + R[1:]           for L, R in splits if R for c in letters]
-    inserts    = [L + c + R               for L, R in splits for c in letters]
+    letters = 'abcdefghijklmnopqrstuvwxyz '+string.punctuation
+    splits = [(word[:i], word[i:]) for i in range(len(word) + 1)]
+    deletes = [L + R[1:] for L, R in splits if R]
+    transposes = [L + R[1] + R[0] + R[2:] for L, R in splits if len(R) > 1]
+    replaces = [L + c + R[1:] for L, R in splits if R for c in letters]
+    inserts = [L + c + R for L, R in splits for c in letters]
     return set(deletes + transposes + replaces + inserts)
 
 
-def sense2vec_get_words(word,s2v):
+def sense2vec_get_words(word, s2v):
     output = []
 
-    word_preprocessed =  word.translate(word.maketrans("","", string.punctuation))
+    word_preprocessed = word.translate(
+        word.maketrans("", "", string.punctuation))
     word_preprocessed = word_preprocessed.lower()
 
     word_edits = edits(word_preprocessed)
@@ -62,35 +61,37 @@ def sense2vec_get_words(word,s2v):
         append_word = each_word[0].split("|")[0].replace("_", " ")
         append_word = append_word.strip()
         append_word_processed = append_word.lower()
-        append_word_processed = append_word_processed.translate(append_word_processed.maketrans("","", string.punctuation))
+        append_word_processed = append_word_processed.translate(
+            append_word_processed.maketrans("", "", string.punctuation))
         if append_word_processed not in compare_list and word_preprocessed not in append_word_processed and append_word_processed not in word_edits:
             output.append(append_word.title())
             compare_list.append(append_word_processed)
-
 
     out = list(OrderedDict.fromkeys(output))
 
     return out
 
-def get_options(answer,s2v):
-    distractors =[]
+
+def get_options(answer, s2v):
+    distractors = []
 
     try:
-        distractors = sense2vec_get_words(answer,s2v)
+        distractors = sense2vec_get_words(answer, s2v)
         if len(distractors) > 0:
             print(" Sense2vec_distractors successful for word : ", answer)
-            return distractors,"sense2vec"
+            return distractors, "sense2vec"
     except:
-        print (" Sense2vec_distractors failed for word : ",answer)
+        print(" Sense2vec_distractors failed for word : ", answer)
 
+    return distractors, "None"
 
-    return distractors,"None"
 
 def tokenize_sentences(text):
     sentences = [sent_tokenize(text)]
     sentences = [y for x in sentences for y in x]
     # Remove any short sentences less than 20 letters.
-    sentences = [sentence.strip() for sentence in sentences if len(sentence) > 20]
+    sentences = [sentence.strip()
+                 for sentence in sentences if len(sentence) > 20]
     return sentences
 
 
@@ -121,24 +122,26 @@ def get_sentences_for_keyword(keywords, sentences):
     return keyword_sentences
 
 
-def is_far(words_list,currentword,thresh,normalized_levenshtein):
+def is_far(words_list, currentword, thresh, normalized_levenshtein):
     threshold = thresh
-    score_list =[]
+    score_list = []
     for word in words_list:
-        score_list.append(normalized_levenshtein.distance(word.lower(),currentword.lower()))
-    if min(score_list)>=threshold:
+        score_list.append(normalized_levenshtein.distance(
+            word.lower(), currentword.lower()))
+    if min(score_list) >= threshold:
         return True
     else:
         return False
 
-def filter_phrases(phrase_keys,max,normalized_levenshtein ):
-    filtered_phrases =[]
-    if len(phrase_keys)>0:
+
+def filter_phrases(phrase_keys, max, normalized_levenshtein):
+    filtered_phrases = []
+    if len(phrase_keys) > 0:
         filtered_phrases.append(phrase_keys[0])
         for ph in phrase_keys[1:]:
-            if is_far(filtered_phrases,ph,0.7,normalized_levenshtein ):
+            if is_far(filtered_phrases, ph, 0.7, normalized_levenshtein):
                 filtered_phrases.append(ph)
-            if len(filtered_phrases)>=max:
+            if len(filtered_phrases) >= max:
                 break
     return filtered_phrases
 
@@ -171,49 +174,49 @@ def get_nouns_multipartite(text):
 
 
 def get_phrases(doc):
-    phrases={}
+    phrases = {}
     for np in doc.noun_chunks:
-        phrase =np.text
+        phrase = np.text
         len_phrase = len(phrase.split())
         if len_phrase > 1:
             if phrase not in phrases:
-                phrases[phrase]=1
+                phrases[phrase] = 1
             else:
-                phrases[phrase]=phrases[phrase]+1
+                phrases[phrase] = phrases[phrase]+1
 
-    phrase_keys=list(phrases.keys())
-    phrase_keys = sorted(phrase_keys, key= lambda x: len(x),reverse=True)
-    phrase_keys=phrase_keys[:50]
+    phrase_keys = list(phrases.keys())
+    phrase_keys = sorted(phrase_keys, key=lambda x: len(x), reverse=True)
+    phrase_keys = phrase_keys[:50]
     return phrase_keys
 
 
-
-def get_keywords(nlp,text,max_keywords,s2v,fdist,normalized_levenshtein,no_of_sentences):
+def get_keywords(nlp, text, max_keywords, s2v, fdist, normalized_levenshtein, no_of_sentences):
     doc = nlp(text)
     max_keywords = int(max_keywords)
 
     keywords = get_nouns_multipartite(text)
     keywords = sorted(keywords, key=lambda x: fdist[x])
-    keywords = filter_phrases(keywords, max_keywords,normalized_levenshtein )
+    keywords = filter_phrases(keywords, max_keywords, normalized_levenshtein)
 
     phrase_keys = get_phrases(doc)
-    filtered_phrases = filter_phrases(phrase_keys, max_keywords,normalized_levenshtein )
+    filtered_phrases = filter_phrases(
+        phrase_keys, max_keywords, normalized_levenshtein)
 
     total_phrases = keywords + filtered_phrases
 
-    total_phrases_filtered = filter_phrases(total_phrases, min(max_keywords, 2*no_of_sentences),normalized_levenshtein )
-
+    total_phrases_filtered = filter_phrases(total_phrases, min(
+        max_keywords, 2*no_of_sentences), normalized_levenshtein)
 
     answers = []
     for answer in total_phrases_filtered:
-        if answer not in answers and MCQs_available(answer,s2v):
+        if answer not in answers and MCQs_available(answer, s2v):
             answers.append(answer)
 
     answers = answers[:max_keywords]
     return answers
 
 
-def generate_questions_mcq(keyword_sent_mapping,device,tokenizer,model,sense2vec,normalized_levenshtein):
+def generate_questions_mcq(keyword_sent_mapping, device, tokenizer, model, sense2vec, normalized_levenshtein):
     batch_text = []
     answers = keyword_sent_mapping.keys()
     for answer in answers:
@@ -222,24 +225,26 @@ def generate_questions_mcq(keyword_sent_mapping,device,tokenizer,model,sense2vec
         text = context + " " + "answer: " + answer + " </s>"
         batch_text.append(text)
 
-    encoding = tokenizer.batch_encode_plus(batch_text, pad_to_max_length=True, return_tensors="pt")
+    encoding = tokenizer.batch_encode_plus(
+        batch_text, pad_to_max_length=True, return_tensors="pt")
 
-
-    print ("Running model for generation")
-    input_ids, attention_masks = encoding["input_ids"].to(device), encoding["attention_mask"].to(device)
+    print("Running model for generation")
+    input_ids, attention_masks = encoding["input_ids"].to(
+        device), encoding["attention_mask"].to(device)
 
     with torch.no_grad():
         outs = model.generate(input_ids=input_ids,
                               attention_mask=attention_masks,
                               max_length=150)
 
-    output_array ={}
-    output_array["questions"] =[]
+    output_array = {}
+    output_array["questions"] = []
 #     print(outs)
     for index, val in enumerate(answers):
-        individual_question ={}
+        individual_question = {}
         out = outs[index, :]
-        dec = tokenizer.decode(out, skip_special_tokens=True, clean_up_tokenization_spaces=True)
+        dec = tokenizer.decode(out, skip_special_tokens=True,
+                               clean_up_tokenization_spaces=True)
 
         Question = dec.replace("question:", "")
         Question = Question.strip()
@@ -247,20 +252,24 @@ def generate_questions_mcq(keyword_sent_mapping,device,tokenizer,model,sense2vec
         individual_question["question_type"] = "MCQ"
         individual_question["answer"] = val
         individual_question["id"] = index+1
-        individual_question["options"], individual_question["options_algorithm"] = get_options(val, sense2vec)
+        individual_question["options"], individual_question["options_algorithm"] = get_options(
+            val, sense2vec)
 
-        individual_question["options"] =  filter_phrases(individual_question["options"], 10,normalized_levenshtein)
+        individual_question["options"] = filter_phrases(
+            individual_question["options"], 10, normalized_levenshtein)
         index = 3
-        individual_question["extra_options"]= individual_question["options"][index:]
+        individual_question["extra_options"] = individual_question["options"][index:]
         individual_question["options"] = individual_question["options"][:index]
         individual_question["context"] = keyword_sent_mapping[val]
-     
-        if len(individual_question["options"])>0:
+
+        if len(individual_question["options"]) > 0:
             output_array["questions"].append(individual_question)
 
     return output_array
 
-def generate_normal_questions(keyword_sent_mapping,device,tokenizer,model):  #for normal one word questions
+
+# for normal one word questions
+def generate_normal_questions(keyword_sent_mapping, device, tokenizer, model):
     batch_text = []
     answers = keyword_sent_mapping.keys()
     for answer in answers:
@@ -269,38 +278,40 @@ def generate_normal_questions(keyword_sent_mapping,device,tokenizer,model):  #fo
         text = context + " " + "answer: " + answer + " </s>"
         batch_text.append(text)
 
-    encoding = tokenizer.batch_encode_plus(batch_text, pad_to_max_length=True, return_tensors="pt")
+    encoding = tokenizer.batch_encode_plus(
+        batch_text, pad_to_max_length=True, return_tensors="pt")
 
-
-    print ("Running model for generation")
-    input_ids, attention_masks = encoding["input_ids"].to(device), encoding["attention_mask"].to(device)
+    print("Running model for generation")
+    input_ids, attention_masks = encoding["input_ids"].to(
+        device), encoding["attention_mask"].to(device)
 
     with torch.no_grad():
         outs = model.generate(input_ids=input_ids,
                               attention_mask=attention_masks,
                               max_length=150)
 
-    output_array ={}
-    output_array["questions"] =[]
-    
-    for index, val in enumerate(answers):
-        individual_quest= {}
-        out = outs[index, :]
-        dec = tokenizer.decode(out, skip_special_tokens=True, clean_up_tokenization_spaces=True)
-        
-        Question= dec.replace('question:', '')
-        Question= Question.strip()
+    output_array = {}
+    output_array["questions"] = []
 
-        individual_quest['Question']= Question
-        individual_quest['Answer']= val
+    for index, val in enumerate(answers):
+        individual_quest = {}
+        out = outs[index, :]
+        dec = tokenizer.decode(out, skip_special_tokens=True,
+                               clean_up_tokenization_spaces=True)
+
+        Question = dec.replace('question:', '')
+        Question = Question.strip()
+
+        individual_quest['Question'] = Question
+        individual_quest['Answer'] = val
         individual_quest["id"] = index+1
         individual_quest["context"] = keyword_sent_mapping[val]
-        
+
         output_array["questions"].append(individual_quest)
-        
+
     return output_array
 
+
 def random_choice():
-    a = random.choice([0,1])
+    a = random.choice([0, 1])
     return bool(a)
-    
--- Questgen/train/train_gpu.py
+++ Questgen/train/train_gpu.py
@@ -10,7 +10,6 @@ from itertools import chain
 from string import punctuation
 
 import nltk
-nltk.download('punkt')
 from nltk.tokenize import sent_tokenize
 
 import pandas as pd
@@ -27,12 +26,14 @@ from transformers import (
     get_linear_schedule_with_warmup
 )
 
+
 def set_seed(seed):
-  random.seed(seed)
-  np.random.seed(seed)
-  torch.manual_seed(seed)
-  if torch.cuda.is_available():
-    torch.cuda.manual_seed_all(seed)
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed_all(seed)
+
 
 set_seed(42)
 
@@ -42,8 +43,10 @@ class T5FineTuner(pl.LightningModule):
         super(T5FineTuner, self).__init__()
         self.hparams = hparams
 
-        self.model = T5ForConditionalGeneration.from_pretrained(hparams.model_name_or_path)
-        self.tokenizer = T5Tokenizer.from_pretrained(hparams.tokenizer_name_or_path)
+        self.model = T5ForConditionalGeneration.from_pretrained(
+            hparams.model_name_or_path)
+        self.tokenizer = T5Tokenizer.from_pretrained(
+            hparams.tokenizer_name_or_path)
 
     def is_logger(self):
         return True
@@ -109,7 +112,8 @@ class T5FineTuner(pl.LightningModule):
                 "weight_decay": 0.0,
             },
         ]
-        optimizer = AdamW(optimizer_grouped_parameters, lr=self.hparams.learning_rate, eps=self.hparams.adam_epsilon)
+        optimizer = AdamW(optimizer_grouped_parameters,
+                          lr=self.hparams.learning_rate, eps=self.hparams.adam_epsilon)
         self.opt = optimizer
         return [optimizer]
 
@@ -122,18 +126,21 @@ class T5FineTuner(pl.LightningModule):
         self.lr_scheduler.step()
 
     def get_tqdm_dict(self):
-        tqdm_dict = {"loss": "{:.3f}".format(self.trainer.avg_loss), "lr": self.lr_scheduler.get_last_lr()[-1]}
+        tqdm_dict = {"loss": "{:.3f}".format(
+            self.trainer.avg_loss), "lr": self.lr_scheduler.get_last_lr()[-1]}
 
         return tqdm_dict
 
     def train_dataloader(self):
-        train_dataset = get_dataset(tokenizer=self.tokenizer, type_path="boolq_train", args=self.hparams)
+        train_dataset = get_dataset(
+            tokenizer=self.tokenizer, type_path="boolq_train", args=self.hparams)
         dataloader = DataLoader(train_dataset, batch_size=self.hparams.train_batch_size, drop_last=True, shuffle=True,
                                 num_workers=4)
         t_total = (
-                (len(dataloader.dataset) // (self.hparams.train_batch_size * max(1, self.hparams.n_gpu)))
-                // self.hparams.gradient_accumulation_steps
-                * float(self.hparams.num_train_epochs)
+            (len(dataloader.dataset) //
+             (self.hparams.train_batch_size * max(1, self.hparams.n_gpu)))
+            // self.hparams.gradient_accumulation_steps
+            * float(self.hparams.num_train_epochs)
         )
         scheduler = get_linear_schedule_with_warmup(
             self.opt, num_warmup_steps=self.hparams.warmup_steps, num_training_steps=t_total
@@ -142,38 +149,44 @@ class T5FineTuner(pl.LightningModule):
         return dataloader
 
     def val_dataloader(self):
-        val_dataset = get_dataset(tokenizer=self.tokenizer, type_path="boolq_val", args=self.hparams)
+        val_dataset = get_dataset(
+            tokenizer=self.tokenizer, type_path="boolq_val", args=self.hparams)
         return DataLoader(val_dataset, batch_size=self.hparams.eval_batch_size, num_workers=4)
+
 
 logger = logging.getLogger(__name__)
 
+
 class LoggingCallback(pl.Callback):
-  def on_validation_end(self, trainer, pl_module):
-    logger.info("***** Validation results *****")
-    if pl_module.is_logger():
-      metrics = trainer.callback_metrics
-      # Log results
-      for key in sorted(metrics):
-        if key not in ["log", "progress_bar"]:
-          logger.info("{} = {}\n".format(key, str(metrics[key])))
+    def on_validation_end(self, trainer, pl_module):
+        logger.info("***** Validation results *****")
+        if pl_module.is_logger():
+            metrics = trainer.callback_metrics
+            # Log results
+            for key in sorted(metrics):
+                if key not in ["log", "progress_bar"]:
+                    logger.info("{} = {}\n".format(key, str(metrics[key])))
 
-  def on_test_end(self, trainer, pl_module):
-    logger.info("***** Test results *****")
+    def on_test_end(self, trainer, pl_module):
+        logger.info("***** Test results *****")
 
-    if pl_module.is_logger():
-      metrics = trainer.callback_metrics
+        if pl_module.is_logger():
+            metrics = trainer.callback_metrics
 
-      # Log and save results to file
-      output_test_results_file = os.path.join(pl_module.hparams.output_dir, "test_results.txt")
-      with open(output_test_results_file, "w") as writer:
-        for key in sorted(metrics):
-          if key not in ["log", "progress_bar"]:
-            logger.info("{} = {}\n".format(key, str(metrics[key])))
-            writer.write("{} = {}\n".format(key, str(metrics[key])))
+            # Log and save results to file
+            output_test_results_file = os.path.join(
+                pl_module.hparams.output_dir, "test_results.txt")
+            with open(output_test_results_file, "w") as writer:
+                for key in sorted(metrics):
+                    if key not in ["log", "progress_bar"]:
+                        logger.info("{} = {}\n".format(key, str(metrics[key])))
+                        writer.write("{} = {}\n".format(
+                            key, str(metrics[key])))
+
 
 args_dict = dict(
-    data_dir="", # path for data files
-    output_dir="", # path to save the checkpoints
+    data_dir="",  # path for data files
+    output_dir="",  # path to save the checkpoints
     model_name_or_path='t5-base',
     tokenizer_name_or_path='t5-base',
     max_seq_length=256,
@@ -187,9 +200,10 @@ args_dict = dict(
     gradient_accumulation_steps=16,
     n_gpu=1,
     early_stop_callback=False,
-    fp_16=False, # if you want to enable 16-bit training then install apex and set this to true
-    opt_level='O1', # you can find out more on optimisation levels here https://nvidia.github.io/apex/amp.html#opt-levels-and-properties
-    max_grad_norm=1.0, # if you enable 16-bit training then set this to a sensible value, 0.5 is a good default
+    fp_16=False,  # if you want to enable 16-bit training then install apex and set this to true
+    opt_level='O1',  # you can find out more on optimisation levels here https://nvidia.github.io/apex/amp.html#opt-levels-and-properties
+    # if you enable 16-bit training then set this to a sensible value, 0.5 is a good default
+    max_grad_norm=1.0,
     seed=42,
 )
 
@@ -197,7 +211,7 @@ train_path = "boolq_data/boolq_train.csv"
 val_path = "boolq_data/boolq_val.csv"
 
 train = pd.read_csv(train_path)
-print (train.head())
+print(train.head())
 
 tokenizer = T5Tokenizer.from_pretrained('t5-base')
 
@@ -226,21 +240,24 @@ class BooleanDataset(Dataset):
         source_ids = self.inputs[index]["input_ids"].squeeze()
         target_ids = self.targets[index]["input_ids"].squeeze()
 
-        src_mask = self.inputs[index]["attention_mask"].squeeze()  # might need to squeeze
-        target_mask = self.targets[index]["attention_mask"].squeeze()  # might need to squeeze
+        # might need to squeeze
+        src_mask = self.inputs[index]["attention_mask"].squeeze()
+        # might need to squeeze
+        target_mask = self.targets[index]["attention_mask"].squeeze()
 
         return {"source_ids": source_ids, "source_mask": src_mask, "target_ids": target_ids, "target_mask": target_mask}
 
     def _build(self):
         for idx in range(len(self.data)):
-            passage,true_false,target = self.data.loc[idx, self.passage_column],self.data.loc[idx, self.true_false], self.data.loc[idx, self.target_column]
+            passage, true_false, target = self.data.loc[idx, self.passage_column], self.data.loc[idx,
+                                                                                                 self.true_false], self.data.loc[idx, self.target_column]
             true_false = str(true_false)
-            if true_false.lower() =="true":
-                true_false ="yes"
+            if true_false.lower() == "true":
+                true_false = "yes"
             else:
                 true_false = "no"
             # input_ = "paraphrase: "+ input_ + ' </s>'
-            input_ = "truefalse: %s passage: %s </s>" % (true_false,passage)
+            input_ = "truefalse: %s passage: %s </s>" % (true_false, passage)
             target = target + " </s>"
 
             # tokenize inputs
@@ -257,7 +274,7 @@ class BooleanDataset(Dataset):
 
 
 dataset = BooleanDataset(tokenizer, 'boolq_data', 'boolq_val', 256)
-print("Val dataset: ",len(dataset))
+print("Val dataset: ", len(dataset))
 
 data = dataset[2]
 print(tokenizer.decode(data['source_ids']))
@@ -266,7 +283,8 @@ print(tokenizer.decode(data['target_ids']))
 if not os.path.exists('t5_boolq'):
     os.makedirs('t5_boolq')
 
-args_dict.update({'data_dir': 'boolq_data', 'output_dir': 't5_boolq', 'num_train_epochs':4,'max_seq_length':256})
+args_dict.update({'data_dir': 'boolq_data', 'output_dir': 't5_boolq',
+                 'num_train_epochs': 4, 'max_seq_length': 256})
 args = argparse.Namespace(**args_dict)
 print(args_dict)
 
@@ -279,29 +297,29 @@ train_params = dict(
     gpus=args.n_gpu,
     max_epochs=args.num_train_epochs,
     early_stop_callback=False,
-    precision= 16 if args.fp_16 else 32,
+    precision=16 if args.fp_16 else 32,
     amp_level=args.opt_level,
     gradient_clip_val=args.max_grad_norm,
     checkpoint_callback=checkpoint_callback,
     callbacks=[LoggingCallback()],
 )
 
+
 def get_dataset(tokenizer, type_path, args):
-  return BooleanDataset(tokenizer=tokenizer, data_dir=args.data_dir, type_path=type_path,  max_len=args.max_seq_length)
+    return BooleanDataset(tokenizer=tokenizer, data_dir=args.data_dir, type_path=type_path,  max_len=args.max_seq_length)
 
 
-
-print ("Initialize model")
+print("Initialize model")
 model = T5FineTuner(args)
 
 trainer = pl.Trainer(**train_params)
 
-print (" Training model")
+print(" Training model")
 trainer.fit(model)
 
-print ("training finished")
+print("training finished")
 
-print ("Saving model")
+print("Saving model")
 model.model.save_pretrained('t5_boolq')
 
-print ("Saved model")
+print("Saved model")
